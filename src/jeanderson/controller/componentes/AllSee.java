81c93a7e7ab713d5786715525b2190b96095469c
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-07 23:28:24 -0300

    Formatado o código.

--- src/jeanderson/controller/componentes/AllSee.java
+++ src/jeanderson/controller/componentes/AllSee.java
@@ -11,35 +11,40 @@ import jeanderson.controller.control.ControlStage;
 
 /**
  * Classe que mantém salvo Classes ControlStage em modo estático.
+ *
  * @author Jeanderson
  */
 public enum AllSee {
-    CONTROLADORES()
-    ;
-        /**
-         * Lista que vai conter as classes.
-         */
-        private final List<ControlStage> controladores;
-        /**
-         * Construtor Padrão.
-         */
-        AllSee(){
-            this.controladores = new ArrayList<>();
-        }
-        /**
-         * Adiciona uma Classe a Lista colocando no index informado.
-         * @param index possição onde será armazenada a classe na lista.
-         * @param controlador Classe que será armazenada.
-         */
-        public void addControlador( int index, ControlStage controlador){
-            this.controladores.add(index, controlador);
-        }
-        /**
-         * Retorna a classe do Index Informado.
-         * @param index Index de consulta.
-         * @return Uma classe ControlStage.
-         */
-        public ControlStage getControlador(int index){
-            return this.controladores.get(index);
-        }
+    CONTROLADORES();
+    /**
+     * Lista que vai conter as classes.
+     */
+    private final List<ControlStage> controladores;
+
+    /**
+     * Construtor Padrão.
+     */
+    AllSee() {
+        this.controladores = new ArrayList<>();
+    }
+
+    /**
+     * Adiciona uma Classe a Lista colocando no index informado.
+     *
+     * @param index possição onde será armazenada a classe na lista.
+     * @param controlador Classe que será armazenada.
+     */
+    public void addControlador(int index, ControlStage controlador) {
+        this.controladores.add(index, controlador);
+    }
+
+    /**
+     * Retorna a classe do Index Informado.
+     *
+     * @param index Index de consulta.
+     * @return Uma classe ControlStage.
+     */
+    public ControlStage getControlador(int index) {
+        return this.controladores.get(index);
+    }
 }
