ac73ea0e7285545b18367aa671e70a8f6218ef5e
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-27 18:22:47 -0300

    Classe de controller default. somente usado quando usuario nao informa o fxml

---
+++ src/jeanderson/controller/DefaultController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeanderson.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import jeanderson.controller.componentes.Inicializador;
+
+/**
+ * Classe de controller sem implementação. Somente é utilizada quando o usuário não passa uma classe de controller proprio.
+ * @author Jeanderson
+ */
+public class DefaultController extends Inicializador{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.err.println("Não foi passado uma classe de Controller! Utilizando uma classe de Controller Default.");
+    }
+    
+}
