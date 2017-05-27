df52a1fb3d4d4a0f87e60b0fe6979c2392806d8b
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-27 18:34:20 -0300

    não é mais necessario.

--- src/jeanderson/controller/componentes/Inicializador.java
+++ src/jeanderson/controller/componentes/Inicializador.java
@@ -1,6 +1,7 @@
 package jeanderson.controller.componentes;
 
 import javafx.fxml.Initializable;
+import jeanderson.controller.control.ControlWindow;
 
 /**
  * Classe que Ajuda na implementação das classes de Controller
@@ -15,8 +16,17 @@ public abstract class Inicializador implements Initializable {
     /**
      * Método que é chamado por padrão, quando abrir a Tela pela segunda vez.
      * Deve ser implementado pela classe herdeira para que todos os campos sejam limpos da tela.
+     * @deprecated Utilize a nova classe para controle de janela, chamada ControlWindow, e utilize no lugar deste método o clearFields.
+     * @see ControlWindow
      */
     public void clearCampos() {
+        //deve ser implementado.
+    }
+    /**
+     * Método que é chamado por padrão, quando abrir a Tela pela segunda vez.
+     * Deve ser implementado pela classe herdeira para que todos os campos sejam limpos da tela.
+     */
+    public void clearFields(){
         //deve ser implementado.
     }
     
@@ -25,8 +35,20 @@ public abstract class Inicializador implements Initializable {
      * O usuario ao abrir a Tela deve informa se este método deve ser chamado ou não. Por padrão ele não
      * é chamado automaticamente.
      * @param enable - Boolean 
+     * @deprecated Utilize a nova classe para controle de janela, chamada ControlWindow, e utilize no lugar deste método o enableFields.
+     * @see ControlWindow
      */
     public void enableCampos(boolean enable){
+        //deve ser implementado.
+    }
+    
+     /**
+     *  Método que ativa campos de uma Tela.
+     * O usuario ao abrir a Tela deve informa se este método deve ser chamado ou não. Por padrão ele não
+     * é chamado automaticamente.
+     * @param enable - Boolean 
+     */
+    public void enableFields(boolean enable){
         //deve ser implementado.
     }
     
