6b5906286b94059067ebdff6b292440e43ee6d83
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-27 18:08:23 -0300

    enum para representar as configuracoes gerais.

---
+++ src/jeanderson/controller/util/Configuration.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeanderson.controller.util;
+
+/**
+ *
+ * @author Jeanderson
+ */
+public enum Configuration {
+    AUTO_ENABLE_CLEAR, AUTO_ENABLE_FIELDS, FULLSCREEN,RESIZABLE,MAXIMIZED;
+}
