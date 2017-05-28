94d7f2409331a80d46edb76b5fdcfe504d19cd62
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-05-28 13:47:02 -0300

    Adicionado mais métodos necessarios.

--- src/jeanderson/controller/componentes/Exibicao.java
+++ src/jeanderson/controller/componentes/Exibicao.java
@@ -14,4 +14,28 @@ public interface Exibicao {
     public void show();
     
     public void show(Class<? extends Inicializador> windowReference);
+    
+    public void showEnableFields(boolean enableFields);
+    
+    public void showEnableFields(boolean enableFields,Class<? extends Inicializador> windowReference);
+    
+    public void showEditMode(Object data);
+    
+    public void showEditMode(Object data, Class<? extends Inicializador> windowReference);
+    
+    public void showEditAndEnable(Object data, boolean enableFields);
+    
+    public void showEditAndEnable(Object data, boolean enableFields, Class<? extends Inicializador> windowReference);
+    
+    public void showSecurityMode(boolean hasAccess, boolean showMessage);
+    
+    public void showSecurityMode(boolean hasAccess, boolean showMessage,Class<? extends Inicializador> windowReference);
+    
+    public void showSecurityAndEdit(boolean hasAccess, boolean showMessage, Object data);
+    
+    public void showSecurityAndEdit(boolean hasAccess, boolean showMessage, Object data, Class<? extends Inicializador> windowReference);
+    
+    public void showSecurityEditAndEnable(boolean hasAccess, boolean showMessage, boolean enableFields, Object data);
+    
+    public void showSecurityEditAndEnable(boolean hasAccess, boolean showMessage, boolean enableFields, Object data, Class<? extends Inicializador> windowReference);
 }
