5f3c80fd0235d05952477f6cd616c7da8ab5f1a8
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-06-15 14:32:38 -0300

    removido funções depreciadas.

--- src/jeanderson/controller/componentes/Inicializador.java
+++ src/jeanderson/controller/componentes/Inicializador.java
@@ -15,48 +15,30 @@ public abstract class Inicializador implements Initializable {
 
     /**
      * Método que é chamado por padrão, quando abrir a Tela pela segunda vez.
-     * Deve ser implementado pela classe herdeira para que todos os campos sejam limpos da tela.
-     * @deprecated Utilize a nova classe para controle de janela, chamada ControlWindow, e utilize no lugar deste método o clearFields.
-     * @see ControlWindow
+     * Deve ser implementado pela classe herdeira para que todos os campos sejam
+     * limpos da tela.
      */
-    public void clearCampos() {
-        //deve ser implementado.
+    public void clearFields() {
+        //deve ser implementado.        
     }
+
     /**
-     * Método que é chamado por padrão, quando abrir a Tela pela segunda vez.
-     * Deve ser implementado pela classe herdeira para que todos os campos sejam limpos da tela.
+     * Método que ativa campos de uma Tela. O usuario ao abrir a Tela deve
+     * informa se este método deve ser chamado ou não. Por padrão ele não é
+     * chamado automaticamente.
+     *
+     * @param enable - Boolean
      */
-    public void clearFields(){
+    public void enableFields(boolean enable) {
         //deve ser implementado.
     }
-    
-    /**
-     *  Método que ativa campos de uma Tela.
-     * O usuario ao abrir a Tela deve informa se este método deve ser chamado ou não. Por padrão ele não
-     * é chamado automaticamente.
-     * @param enable - Boolean 
-     * @deprecated Utilize a nova classe para controle de janela, chamada ControlWindow, e utilize no lugar deste método o enableFields.
-     * @see ControlWindow
-     */
-    public void enableCampos(boolean enable){
-        //deve ser implementado.
-    }
-    
-     /**
-     *  Método que ativa campos de uma Tela.
-     * O usuario ao abrir a Tela deve informa se este método deve ser chamado ou não. Por padrão ele não
-     * é chamado automaticamente.
-     * @param enable - Boolean 
-     */
-    public void enableFields(boolean enable){
-        //deve ser implementado.
-    }
-    
+
     /**
      * Método é chamado quando a Tela é aberta em modo de edição.
+     *
      * @param data - Objeto por parâmetro.
      */
-    public void editMode(Object data){
+    public void editMode(Object data) {
         //deve ser implementado.
     }
 
