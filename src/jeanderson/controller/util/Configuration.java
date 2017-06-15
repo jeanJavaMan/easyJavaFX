f23dc3a9e66dec652b1451130d40700d3fd9227d
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-06-15 15:26:09 -0300

    adicionado novas configurações.

--- src/jeanderson/controller/util/Configuration.java
+++ src/jeanderson/controller/util/Configuration.java
@@ -10,5 +10,28 @@ package jeanderson.controller.util;
  * @author Jeanderson
  */
 public enum Configuration {
-     FULLSCREEN,RESIZABLE,MAXIMIZED;
+    /**
+     * Define se a janela deve abrir em fullscreen. Utilizar Boolean para está definição.
+     */
+    FULLSCREEN, 
+    /**
+     * Define se a janela é redimensionavel  Utilizar Boolean para está definição.
+     */
+    RESIZABLE, 
+    /**
+     * Define se a janela é maximizado. Utilizar Boolean para está definição.
+     */
+    MAXIMIZED, 
+    /**
+     * Define um titulo para a janela. Utilizar String para está definição.
+     */
+    TITLE, 
+    /**
+     * Define um icone para a janela. Utilizar String para está definição.
+     */
+    ICON_URL,
+    /**
+     * Define o arquivo FXML referente a Janela. Utilizar String para está definição.
+     */
+    FXML_URL;
 }
