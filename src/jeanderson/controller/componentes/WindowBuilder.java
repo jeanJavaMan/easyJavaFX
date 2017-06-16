e03c6e7c785607ac9429fed3844c3954aa30c4b4
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-06-16 17:45:27 -0300

    removido after construct

--- src/jeanderson/controller/componentes/WindowBuilder.java
+++ src/jeanderson/controller/componentes/WindowBuilder.java
@@ -54,7 +54,6 @@ public class WindowBuilder {
             controlBuilder.defineHowStaticClass();
         }
         ControlWindow<U> controlWindow = controlBuilder.build();
-        configuration.afterConstruct(controlWindow);
         return controlWindow;
     }
 }
