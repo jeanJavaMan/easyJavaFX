7f831b1b7ba6e5db8d56f2fa67a0db243b33b27d
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-06-18 12:23:00 -0300

    atribuido mais algumas funções necessarias.

--- src/jeanderson/controller/componentes/Configurator.java
+++ src/jeanderson/controller/componentes/Configurator.java
@@ -5,7 +5,6 @@
  */
 package jeanderson.controller.componentes;
 
-import jeanderson.controller.control.ControlWindow;
 import jeanderson.controller.util.ConfigurationEasyJavaFX;
 
 /**
@@ -19,57 +18,140 @@ public abstract class Configurator implements WindowConfiguration{
     private boolean isResizable;
     private boolean isFullscreen;
     private boolean isMaximized;
+    private boolean isAutoNewStage;
+    private boolean staticClass;
 
+    /**
+     * Construtor que já define algumas configurações padrões.
+     */
     public Configurator() {
         this.url_icon = ConfigurationEasyJavaFX.getDEFAULT_URL_ICON();
         this.title = ConfigurationEasyJavaFX.getDEFAULT_TITLE();
         this.isFullscreen = ConfigurationEasyJavaFX.isDEFAULT_FULLSCREEN();
         this.isMaximized = ConfigurationEasyJavaFX.isDEFAULT_MAXIMIZED();
-        this.isResizable = ConfigurationEasyJavaFX.isDEFAULT_RESIZABLE();        
+        this.isResizable = ConfigurationEasyJavaFX.isDEFAULT_RESIZABLE();       
+        this.isAutoNewStage = ConfigurationEasyJavaFX.isDEFAULT_AUTO_NEW_STAGE();
+        this.staticClass = ConfigurationEasyJavaFX.isDEFAULT_STATIC_CLASS();
     }
 
+    /**
+     * Altera URL do Icone referente a Janela.
+     *
+     * @param url_icon  URL do Icone.
+     */
     public void setUrl_icon(String url_icon) {
         this.url_icon = url_icon;
     }
 
+    /**
+     * Altera o Titulo da Janela.
+     * @param title 
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setIsResizable(boolean isResizable) {
+    /**
+     * Informa se a Janela é redimensionavel.
+     *
+     * @param isResizable é redimensionavel;
+     */
+    public void setResizable(boolean isResizable) {
         this.isResizable = isResizable;
     }
 
-    public void setIsFullscreen(boolean isFullscreen) {
+    /**
+     * Informa se é para fazer a exibição em modo fullscreen.
+     * 
+     * @param isFullscreen Exibir em fullscreen.
+     */
+    public void setFullscreen(boolean isFullscreen) {
         this.isFullscreen = isFullscreen;
     }
 
-    public void setIsMaximized(boolean isMaximized) {
+    /**
+     * Informa se é para fazer a exibição em modo maximizado.
+     *
+     * @param isMaximized Exibir maximizado.
+     */
+    public void setMaximized(boolean isMaximized) {
         this.isMaximized = isMaximized;
     }
 
+    public void setAutoNewStage(boolean isAutoNewStage) {
+        this.isAutoNewStage = isAutoNewStage;
+    }
+
+    /**
+     * Define se a Classe deve ser estática ou não.
+     * @param staticMod é estática.
+     */
+    public void setStaticClass(boolean staticMod) {
+        this.staticClass = staticMod;
+    }
+    
+    
+    //get da classe
+
+    /**
+     * Informa se o auto new stage está ativado.
+     * @return Boolean informando se está ativo.
+     */
+    public boolean isAutoNewStage() {
+        return isAutoNewStage;
+    }
+
+    /**
+     * Informa se foi definido a classe ControlWindow como estática.
+     * @return 
+     */
+    public boolean isStaticClass() {
+        return staticClass;
+    }
+    
     //métodos da interface
 
+    /**
+     * Retorna o titulo da janela.
+     *
+     * @return Titulo da janela.
+     */
     @Override
     public String title() {
         return this.title;
     }
-
+ 
+    /**
+     * Retorna a URL do icone referente a janela.
+     * @return URL do icone.
+     */
     @Override
     public String url_Icon() {
         return this.url_icon;
     }
 
+    /**
+     * Informa se a Janela é redimensionavel.
+     * @return Boolean.
+     */
     @Override
     public boolean isResizable() {
        return this.isResizable;
     }
 
+    /**
+     * Informa se é para fazer a exibição em modo fullscreen.
+     * @return Boolean.
+     */
     @Override
     public boolean isFullscreen() {
         return this.isFullscreen;
     }
 
+    /**
+     * Informa se é para fazer a exibição em modo maximizado.
+     * @return Boolean.
+     */
     @Override
     public boolean isMaximized() {
         return this.isMaximized;
