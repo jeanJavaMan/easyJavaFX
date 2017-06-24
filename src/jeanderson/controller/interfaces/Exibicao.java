6b6778929d38dad4618b338b7917b06908439189
Jeanderson <Jeanderson@DESKTOP-10QPC6Q>
2017-06-24 11:53:12 -0300

    Agora será utilizado um Stage passado diretamente, para usar como Stage pai.

--- src/jeanderson/controller/interfaces/Exibicao.java
+++ src/jeanderson/controller/interfaces/Exibicao.java
@@ -5,69 +5,70 @@
  */
 package jeanderson.controller.interfaces;
 
-import jeanderson.controller.componentes.Inicializador;
+import javafx.stage.Stage;
 
 /**
  * Interface de exibição de Telas.
+ *
  * @author Jeanderson
  */
 public interface Exibicao {
-    
+
     public void show();
-    
-    public void show(Class<? extends Inicializador> windowReference);
-    
+
+    public void show(Stage windowReference);
+
     public void showEnableFields(boolean enableFields);
-    
-    public void showEnableFields(boolean enableFields,Class<? extends Inicializador> windowReference);
-    
+
+    public void showEnableFields(boolean enableFields, Stage windowReference);
+
     public void showEditMode(Object data);
-    
-    public void showEditMode(Object data, Class<? extends Inicializador> windowReference);
-    
+
+    public void showEditMode(Object data, Stage windowReference);
+
     public void showEditAndEnable(Object data, boolean enableFields);
-    
-    public void showEditAndEnable(Object data, boolean enableFields, Class<? extends Inicializador> windowReference);
-    
+
+    public void showEditAndEnable(Object data, boolean enableFields, Stage windowReference);
+
     public void showSecurityMode(boolean hasAccess, boolean showMessage);
-    
-    public void showSecurityMode(boolean hasAccess, boolean showMessage,Class<? extends Inicializador> windowReference);
-    
+
+    public void showSecurityMode(boolean hasAccess, boolean showMessage, Stage windowReference);
+
     public void showSecurityAndEdit(boolean hasAccess, boolean showMessage, Object data);
-    
-    public void showSecurityAndEdit(boolean hasAccess, boolean showMessage, Object data, Class<? extends Inicializador> windowReference);
-    
+
+    public void showSecurityAndEdit(boolean hasAccess, boolean showMessage, Object data, Stage windowReference);
+
     public void showSecurityEditAndEnable(boolean hasAccess, boolean showMessage, boolean enableFields, Object data);
-    
-    public void showSecurityEditAndEnable(boolean hasAccess, boolean showMessage, boolean enableFields, Object data, Class<? extends Inicializador> windowReference);
-    
+
+    public void showSecurityEditAndEnable(boolean hasAccess, boolean showMessage, boolean enableFields, Object data, Stage windowReference);
+
     public void showModality();
-    
-     public void showModality(Class<? extends Inicializador> windowReference);
-     
-     public void showEnableFieldsModality(boolean enableFields);
-     
-     public void showEnableFieldsModality(boolean enableFields,Class<? extends Inicializador> windowReference);
-     
-     public void showEditModeModality(Object data);
-     
-     public void showEditModeModality(Object data, Class<? extends Inicializador> windowReference);
-     
-     public void showEditAndEnableModality(Object data, boolean enableFields);         
-    
-    public void showEditAndEnableModality(Object data, boolean enableFields, Class<? extends Inicializador> windowReference);
-    
+
+    public void showModality(Stage windowReference);
+
+    public void showEnableFieldsModality(boolean enableFields);
+
+    public void showEnableFieldsModality(boolean enableFields, Stage windowReference);
+
+    public void showEditModeModality(Object data);
+
+    public void showEditModeModality(Object data, Stage windowReference);
+
+    public void showEditAndEnableModality(Object data, boolean enableFields);
+
+    public void showEditAndEnableModality(Object data, boolean enableFields, Stage windowReference);
+
     public void showSecurityModeModality(boolean hasAccess, boolean showMessage);
-    
-    public void showSecurityModeModality(boolean hasAccess, boolean showMessage,Class<? extends Inicializador> windowReference);
-    
+
+    public void showSecurityModeModality(boolean hasAccess, boolean showMessage, Stage windowReference);
+
     public void showSecurityAndEditModality(boolean hasAccess, boolean showMessage, Object data);
-    
-    public void showSecurityAndEditModality(boolean hasAccess, boolean showMessage, Object data, Class<? extends Inicializador> windowReference);
-    
+
+    public void showSecurityAndEditModality(boolean hasAccess, boolean showMessage, Object data, Stage windowReference);
+
     public void showSecurityEditAndEnableModality(boolean hasAccess, boolean showMessage, boolean enableFields, Object data);
-    
-    public void showSecurityEditAndEnableModality(boolean hasAccess, boolean showMessage, boolean enableFields, Object data, Class<? extends Inicializador> windowReference);
-    
+
+    public void showSecurityEditAndEnableModality(boolean hasAccess, boolean showMessage, boolean enableFields, Object data, Stage windowReference);
+
     public void close();
 }
