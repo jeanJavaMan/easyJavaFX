2bc5f10a36f0c43731defad68a04c343625b6b61
jeanderson <jeanderson@localhost>
2017-06-27 19:43:34 -0300

    anotação para informar que um campo deve ser verificado.

---
+++ src/jeanderson/controller/annotations/ValidateField.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeanderson.controller.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Informa que um campo deve ser validado.
+ * @author jeanderson
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ValidateField {
+    
+}
