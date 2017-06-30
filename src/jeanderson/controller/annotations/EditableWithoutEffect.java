e5921712eb59e82813437f0e8d9b2ec6cab3d6f7
jeanderson <jeanderson@localhost>
2017-06-30 11:12:09 -0300

    nova anotação.

---
+++ src/jeanderson/controller/annotations/EditableWithoutEffect.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeanderson.controller.annotations;
+
+import jeanderson.controller.util.FunctionAnnotations;
+
+/**
+ * Informa que a função EditableFields não tem efeito sobre o componente.
+ * @author jeanderson
+ * @see FunctionAnnotations
+ */
+public @interface EditableWithoutEffect {
+    
+}
