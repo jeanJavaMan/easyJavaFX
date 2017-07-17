38ed070605a7b975bd7dc333e13ea5d081ac7dab
jeanderson <jeanderson@localhost>
2017-07-17 12:01:08 -0300

    trocado o Stage pelo WindowControl

--- src/jeanderson/controller/interfaces/Exibicao.java
+++ src/jeanderson/controller/interfaces/Exibicao.java
@@ -5,7 +5,7 @@
  */
 package jeanderson.controller.interfaces;
 
-import javafx.stage.Stage;
+import jeanderson.controller.control.ControlWindow;
 
 /**
  * Interface de exibição de Telas.
@@ -16,59 +16,59 @@ public interface Exibicao {
 
     public void show();
 
-    public void show(Stage windowReference);
+    public void show(ControlWindow windowReference);
 
     public void showEnableFields(boolean enableFields);
 
-    public void showEnableFields(boolean enableFields, Stage windowReference);
+    public void showEnableFields(boolean enableFields, ControlWindow windowReference);
 
     public void showEditMode(Object data);
 
-    public void showEditMode(Object data, Stage windowReference);
+    public void showEditMode(Object data, ControlWindow windowReference);
 
     public void showEditAndEnable(Object data, boolean enableFields);
 
-    public void showEditAndEnable(Object data, boolean enableFields, Stage windowReference);
+    public void showEditAndEnable(Object data, boolean enableFields, ControlWindow windowReference);
 
     public void showSecurityMode(boolean hasAccess, boolean showMessage);
 
-    public void showSecurityMode(boolean hasAccess, boolean showMessage, Stage windowReference);
+    public void showSecurityMode(boolean hasAccess, boolean showMessage, ControlWindow windowReference);
 
     public void showSecurityAndEdit(boolean hasAccess, boolean showMessage, Object data);
 
-    public void showSecurityAndEdit(boolean hasAccess, boolean showMessage, Object data, Stage windowReference);
+    public void showSecurityAndEdit(boolean hasAccess, boolean showMessage, Object data, ControlWindow windowReference);
 
     public void showSecurityEditAndEnable(boolean hasAccess, boolean showMessage, boolean enableFields, Object data);
 
-    public void showSecurityEditAndEnable(boolean hasAccess, boolean showMessage, boolean enableFields, Object data, Stage windowReference);
+    public void showSecurityEditAndEnable(boolean hasAccess, boolean showMessage, boolean enableFields, Object data, ControlWindow windowReference);
 
     public void showModality();
 
-    public void showModality(Stage windowReference);
+    public void showModality(ControlWindow windowReference);
 
     public void showEnableFieldsModality(boolean enableFields);
 
-    public void showEnableFieldsModality(boolean enableFields, Stage windowReference);
+    public void showEnableFieldsModality(boolean enableFields, ControlWindow windowReference);
 
     public void showEditModeModality(Object data);
 
-    public void showEditModeModality(Object data, Stage windowReference);
+    public void showEditModeModality(Object data, ControlWindow windowReference);
 
     public void showEditAndEnableModality(Object data, boolean enableFields);
 
-    public void showEditAndEnableModality(Object data, boolean enableFields, Stage windowReference);
+    public void showEditAndEnableModality(Object data, boolean enableFields, ControlWindow windowReference);
 
     public void showSecurityModeModality(boolean hasAccess, boolean showMessage);
 
-    public void showSecurityModeModality(boolean hasAccess, boolean showMessage, Stage windowReference);
+    public void showSecurityModeModality(boolean hasAccess, boolean showMessage, ControlWindow windowReference);
 
     public void showSecurityAndEditModality(boolean hasAccess, boolean showMessage, Object data);
 
-    public void showSecurityAndEditModality(boolean hasAccess, boolean showMessage, Object data, Stage windowReference);
+    public void showSecurityAndEditModality(boolean hasAccess, boolean showMessage, Object data, ControlWindow windowReference);
 
     public void showSecurityEditAndEnableModality(boolean hasAccess, boolean showMessage, boolean enableFields, Object data);
 
-    public void showSecurityEditAndEnableModality(boolean hasAccess, boolean showMessage, boolean enableFields, Object data, Stage windowReference);
+    public void showSecurityEditAndEnableModality(boolean hasAccess, boolean showMessage, boolean enableFields, Object data, ControlWindow windowReference);
 
     public void close();
 }
