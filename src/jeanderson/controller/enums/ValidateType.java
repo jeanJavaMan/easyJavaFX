a892abfd554b2d6cc214d46d7398e0386bb615ce
jeand <jeand@DESKTOP-7ETO9UM>
2017-09-02 19:17:49 -0300

    criado enum para identificar o tipo de validação do campo

---
+++ src/jeanderson/controller/enums/ValidateType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeanderson.controller.enums;
+
+/**
+ * Qual tipo de validação deve ser efetuado no campo.
+ * @author jeand
+ */
+public enum ValidateType {
+    NONE,TEL_DIG, CPF,DATAS;
+}
